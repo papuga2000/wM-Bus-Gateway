4d4714c9cc49c83ddf06a30bc095e4db2996c621
Kuba Sawulski <kuba@sawul.ski>
2025-06-17 00:32:38 +0200

    Fix display pages autoloader

--- components/wmbus_gateway/__init__.py
+++ components/wmbus_gateway/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import final_validate
+from esphome.core import CORE
 from esphome.components.binary_sensor import BinarySensor
 from esphome.components.ssd1306_base import SSD1306
 from esphome.components.wmbus_meter.base_sensor import BaseSensor
@@ -14,33 +14,18 @@ CONF_BUTTON_ID = "button_id"
 CODEOWNERS = ["@kubasaw"]
 AUTO_LOAD = ["network"]
 
-REGISTERED_SENSORS = []
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_ID): cv.declare_id(DisplayManager),
         cv.GenerateID(CONF_DISPLAY_ID): cv.use_id(SSD1306),
         cv.GenerateID(CONF_BUTTON_ID): cv.use_id(BinarySensor),
-        cv.Optional(CONF_PAGES, default=REGISTERED_SENSORS): cv.All(
-            lambda x: REGISTERED_SENSORS if x == "all" else x,
+        cv.Optional(CONF_PAGES, default="all"): cv.Any(
+            "all",
             [cv.use_id(BaseSensor)],
         ),
     }
 )
-
-
-def FINAL_VALIDATE_SCHEMA(config):
-    if config[CONF_PAGES]:
-        return
-
-    try:
-        sensors = final_validate.full_config.get().get_config_for_path(["sensor"])
-    except KeyError:
-        sensors = []
-
-    REGISTERED_SENSORS.extend(
-        sensor["id"] for sensor in sensors if sensor.get("platform") == "wmbus_meter"
-    )
 
 
 async def to_code(config):
@@ -50,7 +35,17 @@ async def to_code(config):
         await cg.get_variable(config[CONF_BUTTON_ID]),
     )
 
-    for page in config[CONF_PAGES]:
-        cg.add(manager.add_sensor(await cg.get_variable(page)))
+    sensors = (
+        [
+            sensor["id"]
+            for sensor in CORE.config.get("sensor", [])
+            if sensor.get("platform") == "wmbus_meter"
+        ]
+        if config[CONF_PAGES] == "all"
+        else config[CONF_PAGES]
+    )
+
+    for sensor in sensors:
+        cg.add(manager.add_sensor(await cg.get_variable(sensor)))
 
     await cg.register_component(manager, config)
